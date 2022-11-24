29d1737251164abd3a54b3064b89df981013f889
mohit choudhary <mchoudhary@futurerx.com>
2022-11-24 15:27:40 +0530

    changes

---
+++ src/main/java/com/futurerx/batch/common/Constants.java
@@ -0,0 +1,12 @@
+package com.futurerx.batch.common;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class Constants {
+
+  public static final String STATUS_ACTIVE = "ACTIVE";
+  public static final String STATUS_IN_ACTIVE = "IN_ACTIVE";
+
+  public static final String SQS_TYPE_MEMBER_ELIGIBILITY = "MEMBER_ELIGIBILITY";
+}
