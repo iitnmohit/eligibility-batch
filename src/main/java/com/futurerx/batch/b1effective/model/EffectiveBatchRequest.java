0011d240b40028630c6866114944ab4750239472
mohit choudhary <mchoudhary@futurerx.com>
2022-11-24 15:11:02 +0530

    changes

--- src/main/java/com/futurerx/batch/b1effective/model/EffectiveBatchRequest.java
+++ src/main/java/com/futurerx/batch/b1effective/model/EffectiveBatchRequest.java
@@ -1,15 +1,36 @@
 package com.futurerx.batch.b1effective.model;
 
 import com.futurerx.batch.core.model.AbstractBatchRequest;
+import com.futurerx.batch.member.entity.MemberEligibilityEntity;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.springframework.lang.NonNull;
+
+import java.time.LocalDate;
 
 @Setter
 @Getter
 @SuperBuilder
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class EffectiveBatchRequest extends AbstractBatchRequest {}
+public class EffectiveBatchRequest extends AbstractBatchRequest {
+
+  private MemberEligibilityEntity memberEligibilityEntity;
+  private LocalDate effectiveDate;
+
+  @NonNull
+  public EffectiveBatchRequest cloneFromMemberEligibilityEntity(
+      @NonNull MemberEligibilityEntity memberEligibilityEntity) {
+    return EffectiveBatchRequest.builder()
+        .memberEligibilityEntity(memberEligibilityEntity)
+        .effectiveDate(this.getEffectiveDate())
+        .startTime(this.getStartTime())
+        .endTime(this.getEndTime())
+        .dataSourceType(this.getDataSourceType())
+        .environmentType(this.getEnvironmentType())
+        .build();
+  }
+}
