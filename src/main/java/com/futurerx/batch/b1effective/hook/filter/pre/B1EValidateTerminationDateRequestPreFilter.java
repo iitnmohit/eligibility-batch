0ecc19981fe63369265c06ee552960f6fa751adb
mohit choudhary <mchoudhary@futurerx.com>
2022-11-24 15:15:59 +0530

    changes

---
+++ src/main/java/com/futurerx/batch/b1effective/hook/filter/pre/B1EValidateTerminationDateRequestPreFilter.java
@@ -0,0 +1,42 @@
+package com.futurerx.batch.b1effective.hook.filter.pre;
+
+import com.futurerx.batch.b1effective.hook.AbstractB1EffHook;
+import com.futurerx.batch.b1effective.model.EffectiveBatchRequest;
+import com.futurerx.batch.core.hook.request.filter.IRequestPreFilter;
+import com.futurerx.batch.member.entity.MemberEligibilityEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+@Component
+public class B1EValidateTerminationDateRequestPreFilter extends AbstractB1EffHook
+    implements IRequestPreFilter<EffectiveBatchRequest> {
+
+  public static final int PRIORITY = 1000;
+
+  @Override
+  public int getPriority() {
+    return PRIORITY;
+  }
+
+  @Override
+  public boolean test(@NonNull EffectiveBatchRequest request) {
+    return Optional.of(request)
+        .map(EffectiveBatchRequest::getMemberEligibilityEntity)
+        .filter(mee -> nonNull(mee.getEffectiveDate()))
+        .map(this::isValidEligibility)
+        .orElse(false);
+  }
+
+  @NonNull
+  private boolean isValidEligibility(@NonNull MemberEligibilityEntity memberEligibilityEntity) {
+    return isNull(memberEligibilityEntity.getTerminationDate())
+        || memberEligibilityEntity
+            .getEffectiveDate()
+            .isBefore(memberEligibilityEntity.getTerminationDate());
+  }
+}
